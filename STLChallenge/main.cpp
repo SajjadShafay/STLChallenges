e7e8529022e94f46a657877aa5276fe0282cd037
SajjadShafay <shafaysajjad@outlook.com>
2023-04-06 15:58:29 +0100

    Finished

--- STLChallenge/main.cpp
+++ STLChallenge/main.cpp
@@ -8,16 +8,32 @@
 #include <string>
 #include <iomanip>
 
-bool is_palindrome(const std::string& s)
+bool is_palindrome(const std::string& s)  // s = palindrome || s = racecar
 {
     // You must implement this function.
     // Since we are learning the STL - use a stack and a queue to solve the problem.
-    return false;
+ 
+    std::queue<char> queue_pal; 
+    std::stack<char> stack_pal; 
+
+    for (auto c : s)
+        if (isalpha(c)) {
+            queue_pal.push(std::toupper(c)); // queue_pal = [ palindrome ] || queue_pal = [ racecar ]
+            stack_pal.push(std::toupper(c)); // stack_pal = [ emordnilap ] || stack_pal = [ racecar ]
+        }
+  
+    while (queue_pal.size() > 1 && stack_pal.size() > 1) {
+        if (queue_pal.front() != stack_pal.top())
+            return false; 
+        queue_pal.pop(); 
+        stack_pal.pop(); 
+    }
+    return true;
 }
 
 int main()
 {
-    std::vector<std::string> test_strings{ "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
+    std::vector<std::string> test_strings{"a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
         "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
         "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
    
@@ -27,5 +43,6 @@ int main()
         std::cout << std::setw(8) << std::left << is_palindrome(s)  << s << std::endl;
     }
     std::cout << std::endl;
+
     return 0;
 }
