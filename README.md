0db279534754cf4c7f0038d81a5f0bada9541686
SajjadShafay <shafaysajjad@outlook.com>
2023-04-06 15:14:42 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,67 +1,40 @@
-# STL Challenge  
-## Challenge 3  
-Using std::set and std::map  
+# STL Challenges  
+## Challenge 4 - Stack and Queue Challenge  
 
-This challenge has 2 parts.  
-We will be reading words from a text file provided to you.  
-The text file is named 'words.txt' and contains the fist few  
-paragraphs from the book, "The Wonderful Wizard of Oz", by  
-L. Frank Baum.  
+A Palindrome is a string that reads the same backwards or forwards.  
+Simple examples are:  
+madam  
+bob  
+toot  
+radar  
 
-Part 1:  
+An entire phrase can also be a palindome, for example:  
+A Toyota's a toyota  
+A Santa at NASA  
+A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!  
 
-For part 1 of this challenge, you are to display each unique word  
-in the file and immediately following each word display the number  
-of time it occurs in the text file.  
+For the purposes of this assignment we will only consider alpha characters and omit all other characters.  
+We will also not be considering case.  
+So,  
+A Santa at Nasa, will be processed as:  
+ASANTAATNASA  
 
-The words should be displayed in ascending order.  
+A common method to solve this problem is to compare the string to its reverse and  
+if the are equal then it must be a palindrome. But we will use a deque.  
 
-Here is a sample listing of the first few words:  
+I am providing the main driver for you which will automatically run several test cases.  
+You challenge is to write the following function:  
 
-Word         Count  
-===================  
-Aunt                 5  
-Dorothy           8  
-Dorothy's         1  
-Em                   5  
-Even                1  
-From               1  
+bool is_palindrome(const std::string &s) {  
 
-Please use a map with <string, int> Key/ Value pairs  
+This function will expect a string and it must determine if that string is a palindrome and return true if it is, or false if it is not.  
 
-============================================  
-Part 2:  
+So,  
 
-For part 2 of this challenge, you are to display each unique word  
-in the file and immediately following each word display the line numbers  
-in which that word appears.  
+is_palindrome("A Santa at Nasa");   will return true  
+is_palindrome("Hello");   will return false  
 
-The words should be displayed in ascending order and the line numbers for  
-each word should also be displayed in ascending order.  
-If a word appears more than once on a line then the line number should  
-only appear once.  
+Please use a stack and a queue to solve the problem.  
 
-Here is a sample listing of the first few words:  
-
-Word       Occurrences  
-============================  
-Aunt            [ 2 7 25 29 48 ]  
-Dorothy      [ 1 7 15 29 39 43 47 51 ]  
-Dorothy's   [ 31 ]  
-Em              [ 2 7 25 30 48 ]  
-Even           [ 19 ]  
-From          [ 50 ]  
-
-Please use a map of <string,set<int>> Key/Value pairs  
-
-Hint: consider using stringstream to process words  
-once you read in a line from the file.  
-
-Note: I have provided the basic shell for your program.  
-I have also provided the functions that display the maps  
-as well as the function that cleans the words read.  
-You should call the clean_string function for every word  
-you read from the file.  
-
-Good luck and have fun!!!  
-Don't over think this one -- use the STL!  
+If you need a hint, please as in the Q/A.  
+Good luck and have fun!  
--- STLChallenge/main.cpp
+++ STLChallenge/main.cpp
@@ -1,131 +1,31 @@
 // Section 20
-// Challenge 3  
-// Maps and Sets
-
+// Challenge 4
+// Identifying palindrome strings using a stack and queue
+#include <cctype>
+#include <stack>
+#include <queue>
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <map>
-#include <set>
 #include <string>
 #include <iomanip>
 
-// Used for Part1
-// Display the word and count from the 
-// std::map<std::string, int>
-
-void display_words(const std::map<std::string, int>& words) {
-    std::cout << std::setw(12) << std::left << "\nWord"
-        << std::setw(7) << std::right << "Count" << std::endl;
-    std::cout << "===================" << std::endl;
-    for (auto pair : words)
-        std::cout << std::setw(12) << std::left << pair.first
-        << std::setw(7) << std::right << pair.second << std::endl;
-}
-
-// Used for Part2
-// Display the word and occurences from the 
-// std::map<std::string, std::set<int>>
-
-void display_words(const std::map<std::string, std::set<int>>& words)
+bool is_palindrome(const std::string& s)
 {
-    std::cout << std::setw(12) << std::left << "\nWord"
-        << "Occurrences" << std::endl;
-    std::cout << "=====================================================================" << std::endl;
-    for (auto pair : words) {
-        std::cout << std::setw(12) << std::left << pair.first
-            << std::left << "[ ";
-        for (auto i : pair.second)
-            std::cout << i << " ";
-        std::cout << "]" << std::endl;
-    }
+    // You must implement this function.
+    // Since we are learning the STL - use a stack and a queue to solve the problem.
+    return false;
 }
 
-// This function removes periods, commas, semicolons and colon in 
-// a string and returns the clean version
-std::string clean_string(const std::string& s) {
-    std::string result;
-    for (char c : s) {
-        if (c == '.' || c == ',' || c == ';' || c == ':')
-            continue;
-        else
-            result += c;
+int main()
+{
+    std::vector<std::string> test_strings{ "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
+        "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
+        "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
+   
+    std::cout << std::boolalpha;
+    std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
+    for(const auto& s : test_strings) {
+        std::cout << std::setw(8) << std::left << is_palindrome(s)  << s << std::endl;
     }
-    return result;
-}
-
-// Part1 process the file and builds a map of words and the 
-// number of times they occur in the file
-
-void part1() {
-    std::map<std::string, int> words;
-    std::string line{};
-    std::string word{};
-    std::ifstream in_file{ "words.txt" };
-    if (in_file) {
-
-        // You implement this code
-
-        while (std::getline(in_file, line)) {
-            line = clean_string(line); 
-            std::istringstream ss{ line }; 
-            while (ss >> word) {
-                auto it = words.find(word);
-                if (it != words.end()) {
-                    words.at(word) += 1;
-                }
-                else {
-                    words.insert(std::make_pair(word, 1));
-                }
-                //alternate way
-                //words[word]++; 
-            }
-        }
-
-        in_file.close();
-        display_words(words);
-    }
-    else {
-        std::cerr << "Error opening input file" << std::endl;
-    }
-}
-
-// Part2 process the file and builds a map of words and a 
-// set of line numbers in which the word appears
-void part2() {
-    std::map<std::string, std::set<int>> words;
-    std::string line{};
-    std::string word{};
-    int line_count{}; 
-    std::ifstream in_file{ "words.txt" };
-    if (in_file) {
-
-        // You implement this code
-        while (std::getline(in_file, line)) {
-            line_count++; 
-            line = clean_string(line); 
-            std::istringstream ss{ line }; 
-            while (ss >> word) {
-                auto it = words.find(word); 
-                if (it != words.end())
-                    it->second.insert(line_count); 
-                else {
-                    words[word].insert(line_count);
-                }
-            }
-        }
-
-        in_file.close();
-        display_words(words);
-    }
-    else {
-        std::cerr << "Error opening input file" << std::endl;
-    }
-}
-
-int main() {
-    part1();
-    part2();
+    std::cout << std::endl;
     return 0;
 }
-
